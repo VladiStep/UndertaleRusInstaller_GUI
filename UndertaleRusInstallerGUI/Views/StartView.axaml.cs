b0a7e2ac99b6cddcd60cacf635ab9f550daff9b7
VladiStep <vlad2001.21@mail.ru>
2023-12-02 16:09:20 +0300

    Add a Flowey easter egg for the first screen.

--- UndertaleRusInstallerGUI/Views/StartView.axaml.cs
+++ UndertaleRusInstallerGUI/Views/StartView.axaml.cs
@@ -2,6 +2,11 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using System;
+using System.Threading.Tasks;
 using static UndertaleRusInstallerGUI.OSMethods;
 
 namespace UndertaleRusInstallerGUI.Views;
@@ -9,6 +14,9 @@ namespace UndertaleRusInstallerGUI.Views;
 public partial class StartView : UserControl
 {
     private readonly MainWindow mainWindow;
+    private const string floweyPathPrefix = "avares://UndertaleRusInstallerGUI/Assets/flowey/";
+    private readonly Bitmap[] floweyFrames;
+    private bool floweyWasShown;
 
     public StartView() // For the designer preview
     {
@@ -19,6 +27,18 @@ public partial class StartView : UserControl
         InitializeComponent();
 
         this.mainWindow = mainWindow;
+        try
+        {
+            floweyFrames = new[]
+            {
+                new Bitmap(AssetLoader.Open(new Uri(floweyPathPrefix + "1.png"))),
+                new Bitmap(AssetLoader.Open(new Uri(floweyPathPrefix + "2.png"))),
+                new Bitmap(AssetLoader.Open(new Uri(floweyPathPrefix + "3.png"))),
+                new Bitmap(AssetLoader.Open(new Uri(floweyPathPrefix + "4.png"))),
+                new Bitmap(AssetLoader.Open(new Uri(floweyPathPrefix + "5.png")))
+            };
+        }
+        catch { }
     }
 
     private void UserControl_AttachedToVisualTree(object sender, VisualTreeAttachmentEventArgs e)
@@ -51,5 +71,37 @@ public partial class StartView : UserControl
 
         if (mainWindow.ScriptQuestion("Скопировать ссылку в буфер обмена?"))
             mainWindow.Clipboard.SetTextAsync(url);
+    }
+
+    private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        if (!floweyWasShown && e.HeightChanged && e.NewSize.Height > 520)
+        {
+            floweyWasShown = true;
+
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    foreach (var frame in floweyFrames)
+                    {
+                        await Task.Delay(100);
+                        Dispatcher.UIThread.Invoke(() =>
+                        {
+                            FloweyImage.Source = frame;
+                            FloweyImage.UpdateLayout();
+                        }, DispatcherPriority.Render);
+                    }
+                    
+                    Dispatcher.UIThread.Invoke(() =>
+                    {
+                        FloweyImage.Source = null;
+                        foreach (var img in floweyFrames)
+                            img.Dispose();
+                    });
+                }
+                catch { }
+            });
+        }
     }
 }
