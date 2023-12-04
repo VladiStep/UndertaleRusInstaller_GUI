b401630b339b4cfa531293e8c6308ba11281c171
VladiStep <vlad2001.21@mail.ru>
2023-12-04 16:39:05 +0300

    Delete redundant file.

--- UndertaleRusInstallerGUI/build.bat
+++
@@ -1,6 +0,0 @@
-dotnet restore
-dotnet build UndertaleRusInstallerGUI.Desktop
-dotnet publish UndertaleRusInstallerGUI.Desktop -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output win_x64
-dotnet publish UndertaleRusInstallerGUI.Desktop -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output osx_x64
-dotnet publish UndertaleRusInstallerGUI.Desktop -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output linux_x64
-pause
