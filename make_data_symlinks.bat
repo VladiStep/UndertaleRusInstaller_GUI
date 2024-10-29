a14c3f24fdf6d476b4801134d8fb268cf077c12c
VladiStep <vlad2001.21@mail.ru>
2024-10-29 20:37:50 +0300

    Fix "make_data_symlinks.bat" (again).

--- make_data_symlinks.bat
+++ make_data_symlinks.bat
@@ -1,5 +1,5 @@
 mklink "linux_x64\ru_data.zip" "C:\Users\Влади\Downloads\Undertale Russian\ru_data.zip"
 mklink "osx_x64\ru_data.zip" "C:\Users\Влади\Downloads\Undertale Russian\ru_data.zip"
 mklink "win_x86\ru_data.zip" "C:\Users\Влади\Downloads\Undertale Russian\ru_data.zip"
-mklink "UndertaleRusInstallerGUI.Desktop\bin\x86\Debug\net6.0\ru_data.zip" "C:\Users\Влади\Downloads\Undertale Russian\ru_data.zip"
+mklink "UndertaleRusInstallerGUI.Desktop\bin\x64\Debug\net6.0\ru_data.zip" "C:\Users\Влади\Downloads\Undertale Russian\ru_data.zip"
 pause
