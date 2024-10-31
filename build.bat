13fc0084a5d02e301a7dc3728bb5cdb5b7d12200
VladiStep <vlad2001.21@mail.ru>
2024-11-01 01:11:04 +0300

    Improve "build.bat".

--- build.bat
+++ build.bat
@@ -3,4 +3,17 @@ dotnet build UndertaleRusInstallerGUI.Desktop
 dotnet publish UndertaleRusInstallerGUI.Desktop -c Release -r win-x86 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output win_x86
 dotnet publish UndertaleRusInstallerGUI.Desktop -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output osx_x64
 dotnet publish UndertaleRusInstallerGUI.Desktop -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true --output linux_x64
+
+cd win_x86
+del UndertaleRusInstaller
+rename UndertaleRusInstallerGUI.Desktop.exe UndertaleRusInstaller.exe
+cd ..
+cd osx_x64
+del UndertaleRusInstaller
+rename UndertaleRusInstallerGUI.Desktop UndertaleRusInstaller
+cd ..
+cd linux_x64
+del UndertaleRusInstaller
+rename UndertaleRusInstallerGUI.Desktop UndertaleRusInstaller
+
 pause
